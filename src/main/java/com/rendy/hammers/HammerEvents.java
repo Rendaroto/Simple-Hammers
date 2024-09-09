245ec6c2a4414f7dbd747486312ac875f8b85a33
Rendaroto <57144956+Rendaroto@users.noreply.github.com>
2024-09-09 19:34:07 +0200

    Revert "1.20.1 update"

--- src/main/java/com/rendy/hammers/HammerEvents.java
+++ src/main/java/com/rendy/hammers/HammerEvents.java
@@ -59,7 +59,7 @@ public class HammerEvents {
                          * I check if I can actually break it with my usage
                          *
                          */
-                        if (hardness * 2 >= state.getDestroySpeed(level, pos) && isBestTool(state, level, pos, item, event.getPlayer()) && state.getDestroySpeed(level, pos) >= 0f && item.getItem().getDamage(item) + i < item.getMaxDamage()) {
+                        if (hardness * 2 >= state.getDestroySpeed(level, pos) && isBestTool(state, level, pos, item, event.getPlayer()) && state.getDestroySpeed(level, pos) >= 0f && item.getItem().getDamage(item)+1 < item.getMaxDamage()) {
                             if(notCreativeMode){
                                 state.getBlock().playerDestroy(level, event.getPlayer(), pos, state, level.getBlockEntity(pos), mainHand); //set the action to the block
                                 i+=1; //This makes sense later
@@ -67,12 +67,6 @@ public class HammerEvents {
                             level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState()); //I destroy it
                         }
                     }
-                    item.hurtAndBreak(i, event.getPlayer(), (player) -> {
-                        event.getPlayer().broadcastBreakEvent(event.getPlayer().getUsedItemHand());
-                    });
-
-                    item.hurtAndBreak(-1, event.getPlayer(), (player) -> {
-                    });
                 }
             }
         }
