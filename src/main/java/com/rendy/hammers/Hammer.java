f1f17f089a8516d100ceddbb767ea230acbe6a7a
Rendy <=>
2024-09-09 19:30:47 +0200

    1.20.1 update

--- src/main/java/com/rendy/hammers/Hammer.java
+++ src/main/java/com/rendy/hammers/Hammer.java
@@ -2,7 +2,7 @@ package com.rendy.hammers;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.Tiers;
+import com.rendy.hammers.HammerTier;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -14,22 +14,22 @@ public class Hammer {
 
     //Passing to the register name, supplier () -> (Tier, attackModifier, attackSpeed, properties)
     public static final RegistryObject<HammerItem> WOODEN_HAMMER = HAMMER_ITEMS.register  ("wooden_hammer",
-            () -> new HammerItem(Tiers.WOOD,1,-2.8f,
+            () -> new HammerItem(HammerTier.WOODEN_HAMMER,1,-2.8f,
                     new Item.Properties()));
     public static final RegistryObject<HammerItem> STONE_HAMMER = HAMMER_ITEMS.register  ("stone_hammer",
-            () -> new HammerItem(Tiers.STONE,1,-2.8f,
+            () -> new HammerItem(HammerTier.STONE_HAMMER,1,-2.8f,
                     new Item.Properties()));
     public static final RegistryObject<HammerItem> GOLDEN_HAMMER = HAMMER_ITEMS.register  ("golden_hammer",
-            () -> new HammerItem(Tiers.GOLD,1,-2.8f,
+            () -> new HammerItem(HammerTier.GOLD_HAMMER,1,-2.8f,
                     new Item.Properties()));
     public static final RegistryObject<HammerItem> IRON_HAMMER = HAMMER_ITEMS.register  ("iron_hammer",
-            () -> new HammerItem(Tiers.IRON,1,-2.8f,
+            () -> new HammerItem(HammerTier.IRON_HAMMER,1,-2.8f,
                     new Item.Properties()));
     public static final RegistryObject<HammerItem> DIAMOND_HAMMER = HAMMER_ITEMS.register  ("diamond_hammer",
-            () -> new HammerItem(Tiers.DIAMOND,1,-2.8f,
+            () -> new HammerItem(HammerTier.DIAMOND_HAMMER,1,-2.8f,
                     new Item.Properties()));
     public static final RegistryObject<HammerItem> NETHERITE_HAMMER = HAMMER_ITEMS.register  ("netherite_hammer",
-            () -> new HammerItem(Tiers.NETHERITE,1,-2.8f,
+            () -> new HammerItem(HammerTier.NETHERITE_HAMMER,1,-2.8f,
                     new Item.Properties().fireResistant().rarity(Rarity.EPIC)));
 
 }
